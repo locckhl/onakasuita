079549655911dbb4804c5c2d240df0b7cdc9f2ee
locckhl <locckhl123@gmail.com>
2021-06-02 12:59:14 +0700

    Add review detail

---
+++ src/components/ReviewDetail/ReviewDetail.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./index.scss"
+import sushi from "../../assets/images/sushi.jpg"
+
+export default function ReviewDetail() {
+    return (
+        <div className="review-detail">
+            <div className="review-detail-img">
+                <a className="review-detail-img__container" href="/review-detail">
+                    <img src={sushi} alt="sushi" />
+                </a>
+            </div>
+            <div className="review-detail-text">
+                <div className="review-detail-text__header">Sushi</div>
+                <div className="review-detail-text__body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua <a href="/review-detail">More</a></div>
+                <div className="review-detail-text__time">May 7, 2021</div>
+            </div>
+        </div>
+    )
+}
