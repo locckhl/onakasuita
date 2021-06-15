719166ad6122bf1a7375a674259c2a0356b24d94
locckhl <locckhl123@gmail.com>
2021-06-15 13:59:10 +0700

    Update review list

--- src/components/ReviewItemList/ReviewItemList.jsx
+++ src/components/ReviewItemList/ReviewItemList.jsx
@@ -1,23 +1,18 @@
-import React from 'react'
-import ReviewItem from '../ReviewItem/ReviewItem'
-export default function ReviewItemList() {
-    return (
-        <div className="new-review-list row row-cols-1 row-cols-lg-4 justify-content-center">
-            <div className="col mb-5">
-              <ReviewItem />
-            </div>
-            <div className="col mb-5">
-              <ReviewItem />
-            </div>
-            <div className="col mb-5">
-              <ReviewItem />
-            </div>
-            <div className="col mb-5">
-              <ReviewItem />
-            </div>
-            <div className="col mb-5">
-              <ReviewItem />
-            </div>
-          </div>
-    )
+import React, { useState, useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
+import ReviewItem from "../ReviewItem/ReviewItem";
+export default function ReviewItemList({ items }) {
+  const [data, setData] = useState(items);
+  useEffect(() => {
+    console.log(items);
+    return () => {};
+  }, []);
+  if (!items) return <Skeleton count={5} />;
+  return (
+    <div className="new-review-list row row-cols-1 row-cols-lg-4 justify-content-center">
+      {items.map((item) => (
+        <ReviewItem item={item}/>
+      ))}
+    </div>
+  );
 }
