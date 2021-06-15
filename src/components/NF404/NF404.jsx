9d05ac90d93610dbd2cfb53dc7d5a0a58441adf9
locckhl <locckhl123@gmail.com>
2021-06-15 17:58:55 +0700

    Update admin status

---
+++ src/components/NF404/NF404.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.scss"
+export default function NF404() {
+  return (
+    <div id="nf">
+    	<div class="fof">
+        		<h1>Error: 404 not found </h1>
+    	</div>
+</div>
+  );
+}
