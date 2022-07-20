0c3ea43ed7fc935a198c597d8bb23fdb2ddb448a
locckhl <locckhl123@gmail.com>
2022-07-20 14:59:40 +0700

    Add bundle.jsv1

--- src/pages/ReviewList/ReviewList.jsx
+++ src/pages/ReviewList/ReviewList.jsx
@@ -5,20 +5,19 @@ import { getReviews } from "../../lib/api/reviews";
 import { Helmet } from "react-helmet";
 
 export default function ReviewList() {
-  const [reviews, setReviews] = useState([])
-  const [searchInput, setSearchInput] = useState("")
+  const [reviews, setReviews] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
   const [commentSort, setCommentSort] = useState("asc");
-  const [datesort, setdateSort] = useState('asc');
+  const [datesort, setdateSort] = useState("asc");
 
   useEffect(() => {
-    fetchData()
+    fetchData();
+  }, []);
 
-  }, [])
-
-  const fetchData = async () =>{
-    const res = await getReviews()
-    setReviews(res)
-  }
+  const fetchData = async () => {
+    const res = await getReviews();
+    setReviews(res);
+  };
 
   const filteredByTitle = reviews.filter((item) => {
     return item.title.match(searchInput);
@@ -38,13 +37,13 @@ export default function ReviewList() {
   //     return a.comments - b.comments;
   // });
 
-  if(!reviews) return ( <div>loading</div> )
+  if (!reviews) return <div>loading</div>;
 
   return (
     <div className="container my-5 overflow-hidden">
       <Helmet>
-          <title>Review List</title>
-        </Helmet>
+        <title>Review List</title>
+      </Helmet>
       <div className="new-review-list__header fs-1 text-center my-3">
         Review list
       </div>
@@ -58,26 +57,31 @@ export default function ReviewList() {
             aria-label="serach"
             aria-describedby="addon-wrapping"
             value={searchInput}
-            onChange={(e)=>{
-              setSearchInput(e.target.value)
+            onChange={(e) => {
+              setSearchInput(e.target.value);
             }}
           />
         </div>
         <div className="float-end">
-            <div className="row">
-                {/* <div className={`col filter-element ${commentSort}`} onClick={(e)=>{
+          <div className="row">
+            {/* <div className={`col filter-element ${commentSort}`} onClick={(e)=>{
                   commentSort === "asc" ? setCommentSort("desc") : setCommentSort("asc")
                 }}>Comments</div> */}
-                <div className={`col filter-element ${datesort}`} onClick={(e)=>{
-                  datesort === "asc" ? setdateSort("desc") : setdateSort("asc")
-                }}>Dates</div>
+            <div
+              className={`col filter-element ${datesort}`}
+              onClick={(e) => {
+                datesort === "asc" ? setdateSort("desc") : setdateSort("asc");
+              }}
+            >
+              Dates
             </div>
+          </div>
         </div>
       </div>
 
       <div className="review-item-list-container">
-          <ReviewItemList items={sortByTime}/>
-        </div>
+        <ReviewItemList items={sortByTime} />
+      </div>
     </div>
   );
 }
